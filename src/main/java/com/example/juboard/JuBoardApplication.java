d126fe98937fbd1e0105a2b6367f74b011e9f10b
wise_JG <59231743+Hju95@users.noreply.github.com>
2025-05-12 00:18:57 +0900

    Init Project

---
+++ src/main/java/com/example/juboard/JuBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.juboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/juboard/JuBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.juboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
